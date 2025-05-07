e2402be0eb886bdf60939944a8e7979abc889501
isabela Barcellos Freire <isabelabarcellos0812@gmail.com>
2025-05-07 14:08:47 -0300

    iniciando projeto com o spring

---
+++ src/main/java/br/com/fiap/espb/checkpoint/receitas/ReceitasApplication.java
@@ -0,0 +1,17 @@
+package br.com.fiap.espb.checkpoint.receitas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReceitasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReceitasApplication.class, args);
+	}
+
+}
+
+
+
+
